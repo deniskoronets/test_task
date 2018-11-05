0e4412570696fa88bd27424f3cb4d400afba846d
Denis Koronets <deniskoronets@woo.zp.ua>
2018-11-05 12:29:30 +0200

    Gitignore

--- config/params-local.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'jwtSecret' => 'qweqweqwewqiofrmwgn237bfvb3672v2q1',
-    'githubToken' => '214bfb9ef4976f309951258d67d3b1b5d97cc932',
-    'openWeatherKey' => 'ecc485bb2276a06522c66000d7ed23c3',
-];
